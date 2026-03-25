a61b0c7b78276997423b43363a6ffbf96d8fd86e
CYRUS-pinto <PINTOCYRUS@GMAIL.COM>
2026-03-26 00:02:05 +0530

    chore: ignore nextjs cache

--- .next/dev/static/chunks/src_app_page_tsx_e68ab100._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_f621e084._.js",
-  "static/chunks/src_ad341f27._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/src_app_student_page_tsx_1cf6b850._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_321f2664._.js",
-  "static/chunks/src_4f4666cf._.js"
-],
-    source: "dynamic"
-});
